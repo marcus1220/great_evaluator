ff894f9c89200a31cd8fc3bb1ac650f6fa058536
marcus1220 <marcusma7600@gmail.com>
2023-04-22 12:27:20 -0700

    it should be able to work but by some how it cant

--- great_evaluator.py
+++ great_evaluator.py
@@ -19,8 +19,15 @@ with open("data.csv","r") as file:
         print(student_id, assignment_name, grade)
         
         # if this assignment have't been added to the grade dictionary, then add it into.
-        if assignment_name not in grade:
+        if assignment_name not in grades:
             grades[assignment_name] = []
 
         # add grade to the list of grade for this assignment
         grades[assignment_name].append(grade) #we may need (int(grade))
+
+    # iterat over each assinment iterats the dictionary
+    for assignment_name, grades_list in grades.items():
+
+        # calcula the average grade in the assignment
+        average_grade = sum(grades_list)/len(grades_list)
+        print(f"{assignment_name}:{average_grade}")
