64f509e812cc3a7a9dac72edd849a7ca30491549
marcus1220 <marcusma7600@gmail.com>
2023-04-22 11:37:34 -0700

    reading data from file

--- great_evaluator.py
+++ great_evaluator.py
@@ -1,4 +1,20 @@
 import csv
+
 #opening the csv file
 with open("data.csv","r") as file:
+
     # creat a csv reader
+    reader = csv.reader(file)
+
+    # init a dictionary to keep the great for each assignment
+    grade = {}
+
+    # iterating over eash row in the csv file
+    for row in reader:
+
+        # excting the student Id assignment name and grade
+        student_id, assignment_name, grade = row
+
+        # printing data and file
+        print(student_id, assignment_name, grade)
+        
